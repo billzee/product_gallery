9088a864070717e115bb1e0c42ae88523799a5a4
Guilherme Zordan <guilhermezordan@outlook.com>
2016-10-14 20:29:32 -0300

    salvando produto com images

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -6,21 +6,45 @@ class ProductTest < ActiveSupport::TestCase
     @image = images(:one)
   end
 
+  def has_image(product)
+    image = Image.new(link: 'teste', product_id: product.id)
+    image.save
+    if image.product_id == product.id
+      return true
+    else
+      return false
+    end
+  end
+
+  def has_not_image(product)
+    image = Image.new(link: 'teste', product_id: products(:one))
+    image.save
+    if image.product_id == product.id
+      return true
+    else
+      return false
+    end
+  end
+
   test "ct_002" do
     product = Product.new(name: 'teste', category_id: @category.id)
-    product.save
-    assert product.save, "Saved the product with mandatory fields"
+    assert product.save && has_image(product), "Saved the product with mandatory fields"
   end
 
   test "ct_003" do
     product = Product.new()
-    assert_not product.save, "Saved the product with blank fields"
+    assert product.save, "Didnt save the product with blank fields"
+  end
+
+  test "ct_004" do
+    product = Product.new(name: 'teste', category_id: @category.id)
+    assert product.save && has_not_image(product), "Didnt save the product without image"
   end
 
   test "ct_005" do
     product = Product.new(name: 'teste', description: 'teste', link: 'teste', category_id: @category.id)
     product.save
-    assert product.save, "Saved the product with mandatory fields"
+    assert product.save && has_image(product), "Saved the product with all fields"
   end
 
   test "ct_006" do
@@ -28,10 +52,8 @@ class ProductTest < ActiveSupport::TestCase
     assert Product.update(product.id, name: 'aaaaaa', description: 'aaaaaa', link: 'aaaaaa', category_id: @category.id), "Product updated"
   end
 
-
-  # test "ct_003" do
-  #   product = Product.new(images: )
-  #   assert_not product.save, "Saved the product with mandatory fields"
-  # end
-
+  test "ct_007" do
+    product = products(:one)
+    assert product.destroy, "Product deleted"
+  end
 end
