1ed0becb8edb355a273c97912b940f96609b1a98
Guilherme Zordan <guilhermezordan@outlook.com>
2016-11-09 21:45:11 -0200

    a

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,14 +4,34 @@ RSpec.describe Product, type: :model do
 
   describe "the signin process", :type => :feature do
     before :each do
-      Admin.create(username: 'teste', password: '123456789')
+      admin = Admin(username: 'teste', password: '123456789')
+      admin.save
     end
 
-    it "signs me in" do
+    it "CT_001" do
       visit '/admins/sign_in'
       within("#new_admin") do
         fill_in 'admin[username]', with: 'teste'
         fill_in 'admin[password]', with: '123456789'
+      end
+      click_button 'Entrar'
+      expect(page).to have_content 'sucesso'
+    end
+  end
+
+  describe "the product maintaining process", :type => :feature do
+    before :each do
+      admin = Admin(username: 'teste', password: '123456789')
+      admin.save
+      login_as(admin, :scope => :user, :run_callbacks => false)
+    end
+
+    it "CT_002" do
+      visit '/products/new'
+      within("#new_product") do
+        fill_in 'product[name]', with: 'teste'
+        fill_in 'product[category_id]', with: '1'
+        click_button '.custom-file-input'
       end
       click_button 'Entrar'
       expect(page).to have_content 'sucesso'
